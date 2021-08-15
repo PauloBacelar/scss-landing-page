d0531a9073c0af1eeffdda448638c2712fe4fd20
Paulo Guilherme Bacelar Andrade <paulobacelara@gmail.com>
2021-08-15 16:54:44 -0300

    Burger changes to cross (X) when clicked

--- script.js
+++ script.js
@@ -6,6 +6,14 @@ const navSlide = () => {
   // Toggle .nav-active class
   burger.addEventListener("click", () => {
     nav.classList.toggle("nav-active");
+
+    if (nav.classList.contains("nav-active")) {
+      burger.classList.remove("fa-bars");
+      burger.classList.add("fa-times");
+    } else {
+      burger.classList.add("fa-bars");
+      burger.classList.remove("fa-times");
+    }
   });
 };
 
