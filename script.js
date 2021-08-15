ed422b53f7b79871c35c0d7478079bf584ab3d0a
Paulo Guilherme Bacelar Andrade <paulobacelara@gmail.com>
2021-08-15 16:48:29 -0300

    Navbar can be toggled in mobile (no animation yet)

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log("Hello, World!");
+const navSlide = () => {
+  // Variables
+  const burger = document.querySelector(".burger");
+  const nav = document.querySelector(".navbar__menu");
+
+  // Toggle .nav-active class
+  burger.addEventListener("click", () => {
+    nav.classList.toggle("nav-active");
+  });
+};
+
+navSlide();
